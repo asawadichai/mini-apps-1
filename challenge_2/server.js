55eab5e7297756951a7e29fd0581b86896112447
asawadichai <sawadichai.andrew@gmail.com>
2022-05-18 20:47:28 -0700

    created html page, server, middleware function to read JSON.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended:true}));
+const port = 3000;
 
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
 
+app.post('/', (req, res) => {
+  console.log(req.body);
+  console.log('data', JSON.parse(req.body.data));
+
+  req.on('end', () => {
+    res.end();
+  })
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
