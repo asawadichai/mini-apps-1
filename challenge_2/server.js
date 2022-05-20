792996585c00d124591eadbd79d5e655b609c3c2
asawadichai <sawadichai.andrew@gmail.com>
2022-05-20 12:19:20 -0700

    Complete json to csv webpage using textarea.

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 var app = express();
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended:true}));
+
 const port = 3000;
 
 app.listen(port, () => {
@@ -10,14 +12,52 @@ app.listen(port, () => {
 })
 
 app.post('/', (req, res) => {
-  console.log(req.body);
-  console.log('data', JSON.parse(req.body.data));
+  var input = req.body.data;
+  var report = generateReport(req.body.data);
+  console.log(report);
 
-  req.on('end', () => {
-    res.end();
-  })
+  res.send(`<!DOCTYPE html>
+  <html>
+    <head>
+      <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+      <title>Challenge 2: CSV Report Generator</title>
+    </head>
+    <body>
+      <p><strong>CSV Report Generator</strong></p>
+      <p>Description: Generates CSV reports from JSON data</p>
+      <form action="/" method="POST" class="form">
+        <label>Enter JSON:</label>
+        <textarea id="data" name="data" rows="15" cols="30">${input}</textarea>
+        <input type="submit" value="submit">
+        <label> CSV Report:</label>
+        <textarea id="report" name="report" rows="15" cols="60">${report}</textarea>
+      </form>
+      <script src="app.js"></script>
+    </body>
+  </html>`);
 })
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
+
+var generateReport = function (data) {
+  var obj = JSON.parse(data);
+  var keys = Object.keys(obj).slice(0, -1);
+  var csv = '';
+  keys.forEach(key => {
+    csv += key + ',';
+  });
+  csv = csv.slice(0, -1) + '\n';
+  var generateCSV = function (obj) {
+    keys.forEach(key => {
+      csv += obj[key] + ',';
+    })
+    csv = csv.slice(0, -1) + '\n';
+    if (obj['children']) {
+      obj['children'].forEach(child => generateCSV(child));
+    }
+  }
+  generateCSV(obj);
+  return csv;
+}
