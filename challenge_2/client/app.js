699192cb1cc003c0568c6eb704c7711c9e9d62ac
asawadichai <sawadichai.andrew@gmail.com>
2022-05-21 14:11:09 -0700

    Complete CSV-Report-Generator Bare Minimum Requirements

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,63 @@
-var submission;
+$(document).ready(function() {
+  $("#download").click(function() {
+    $.ajax({
+      url: "/",
+      type: "GET",
+      success: function(response){
+        if(response != 0){
+          console.log('file uploaded');
+        } else {
+          console.log('file not uploaded');
+        }
+      }
+    })
+  })
 
+
+  $("#upload").click(function() {
+    var file = $('#file')[0].files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = function(event) {
+      var content = event.target.result;
+      console.log(content);
+
+      $.ajax({
+        url: "/",
+        type: "POST",
+        data: content,
+        contentType: 'application/json',
+        success: function(response){
+          if(response != 0){
+            console.log('file uploaded');
+          } else {
+            console.log('file not uploaded');
+          }
+        }
+      })
+    }
+    reader.readAsText(file);
+  })
+})
+
+// const fileSelect = document.getElementById('file');
+// fileSelect.addEventListener('change', read, false);
+
+// function read() {
+//   var fileInput = this.files;
+//   var file = fileInput[0];
+
+//   var reader = new FileReader();
+
+//   console.log('reading file', fileInput);
+
+//   reader.onload = function(event) {
+//     var content = event.target.result;
+//     console.log(content);
+//     document.getElementById('data').innerText = content;
+//   }
+
+//   reader.readAsText(file);
+//   console.log('result', reader.result);
+// }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
 
 var app = express();
 app.use(express.static('client'));
@@ -11,35 +13,86 @@ app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
 
+app.get('/csvreport', (req, res) => {
+  res.sendFile(path.join(__dirname, 'csvreport.csv'), (err) => {
+    if(err) {
+      throw err;
+    } else {
+      console.log('sent file');
+    }
+  })
+})
+
 app.post('/', (req, res) => {
-  var input = req.body.data;
-  var report = generateReport(req.body.data);
-  console.log(report);
+  //receive json from webpage
+  var data = '';
+  req.on('data', chunk => {
+    data += chunk;
+  })
+  req.on('end', () => {
+    res.writeHead(201);
+    var csv = generateReport(data);
+    console.log('csv', csv);
+    fs.writeFile('csvreport.csv', csv, function(err) {
+      if(err) throw err;
+      console.log('saved');
+    })
 
-  res.send(`<!DOCTYPE html>
-  <html>
-    <head>
-      <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
-      <title>Challenge 2: CSV Report Generator</title>
-    </head>
-    <body>
-      <p><strong>CSV Report Generator</strong></p>
-      <p>Description: Generates CSV reports from JSON data</p>
-      <form action="/" method="POST" class="form">
-        <label>Enter JSON:</label>
-        <textarea id="data" name="data" rows="15" cols="30">${input}</textarea>
-        <input type="submit" value="submit">
-        <label> CSV Report:</label>
-        <textarea id="report" name="report" rows="15" cols="60">${report}</textarea>
-      </form>
-      <script src="app.js"></script>
-    </body>
-  </html>`);
+    res.end();
+  })
+
+  // res.send(`<!DOCTYPE html>
+  // <html>
+  //   <head>
+  //     <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+  //     <title>Challenge 2: CSV Report Generator</title>
+  //   </head>
+  //   <body>
+  //     <p><strong>CSV Report Generator</strong></p>
+  //     <p>Description: Generates CSV reports from JSON data</p>
+  //     <form action="/" method="POST" enctype="multipart/form-data" class="form">
+  //       <label> CSV Report:</label>
+  //       <textarea id="report" name="report" rows="15" cols="60">${data}</textarea>
+  //       <p><label>Upload JSON:</label><input type="file" id="file" name="jsonfile" accept=".json">
+  //       <input type="button" value="upload" id="upload"></p>
+  //     </form>
+  //     <script src="app.js"></script>
+  //   </body>
+  // </html>`);
+
+
+  // if(input){
+  //   var report = generateReport(input);
+  //   res.send(`<!DOCTYPE html>
+  //   <html>
+  //     <head>
+  //       <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
+  //       <title>Challenge 2: CSV Report Generator</title>
+  //     </head>
+  //     <body>
+  //       <p><strong>CSV Report Generator</strong></p>
+  //       <p>Description: Generates CSV reports from JSON data</p>
+  //       <form action="/" method="POST" class="form">
+  //         <label>Enter JSON:</label>
+  //         <textarea id="data" name="data" rows="15" cols="30">${input}</textarea>
+  //         <input type="submit" value="submit">
+  //         <label> CSV Report:</label>
+  //         <textarea id="report" name="report" rows="15" cols="60">${report}</textarea>
+  //       </form>
+  //       <form action="/" method="POST" enctype="multipart/form-data" class="form">
+  //         <label>Upload JSON:</label>
+  //         <input type="file" id="file" name="jsonfile" accept=".json">
+  //         <input type="submit" value="upload">
+  //         <label> CSV Report:</label>
+  //         <textarea id="report" name="report" rows="15" cols="60"></textarea>
+  //       </form>
+  //       <script src="app.js"></script>
+  //     </body>
+  //   </html>`);
+  // }
+
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
 
 var generateReport = function (data) {
   var obj = JSON.parse(data);
