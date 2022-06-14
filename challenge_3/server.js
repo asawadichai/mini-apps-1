6e2908d0065962597792aed2597b325195b48dbe
asawadichai <sawadichai.andrew@gmail.com>
2022-06-13 20:50:52 -0700

    Initialize checkout project files.

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,11 @@
+const express = require('express');
+let app = express();
 
+app.use(express.static('public'));
+
+let port = 3100;
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
 
